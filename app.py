fef6daea724b323c2c3f71dfd7cecf84c518f3ad
Yulin Hou <yhou@mycaf.com>
2025-03-06 12:50:00 -0800

    update

--- app.py
+++ app.py
@@ -110,8 +110,8 @@ def process_pdf_based_on_template(pdf_file, template_version):
     return df,export_name
 
 def main():
-    st.set_page_config(page_title="PDF Transformer", page_icon="⚡" )
-    st.title("Welcome to MyCAF PDF Transformer!")
+    st.set_page_config(page_title="PDF Convert", page_icon="⚡" )
+    st.title("Welcome to MyCAF PDF Convert!")
     
     uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
     
